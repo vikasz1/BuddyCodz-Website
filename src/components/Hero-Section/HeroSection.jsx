9e12de770de31875e31fff2794609bf952a6e9e2
vikasz1 <mauryvikas12345@gmail.com>
2022-05-29 15:14:06 +0530

    First Commit

---
+++ src/components/Hero-Section/HeroSection.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../Hero-Section/learn-code.png";
+// import heroImg from "../../assests/images/hero-img1.png";
+import "./hero-section.css";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="hero__content">
+              <h2 className="mb-4 hero__title">
+                Anytime Anywhere <br /> Learn to code <br />
+              </h2>
+              <p className="mb-5">
+              Make your child learn how to<br/> create their own apps, <br/>games and website
+              </p>
+            </div>
+            <div className="search">
+              <input type="text" placeholder="Search" />
+              <button className="btn">Search</button>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <img src={heroImg} alt="" className="w-100 hero__img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default HeroSection;
